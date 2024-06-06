6ffcda35e1619f228c9353ffbd554a7c2378c074
Catelyn Richard <catelyn.richard@yahoo.com>
2024-06-06 12:09:27 -0400

    fixed preloader and no results added

---
+++ src/components/NoResults/NoResults.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./NoResults.css";
+
+export default function NoResults() {
+  return (
+    <div className="noresults">
+      <p className="">Nothing found</p>
+      <p className="">Sorry, but nothing matched your search terms</p>
+    </div>
+  );
+}
